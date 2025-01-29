cf8d8a313b2648d1a7c7509b08880ce1f7c301ba
dilankayasuru <dilankayasuru001@gmail.com>
2025-01-29 10:45:53 +0530

    update auth api to get device name

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,10 +17,11 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         try {
-            Validator::make($request->only('name', 'email', 'password'), [
+            Validator::make($request->only('name', 'email', 'password', 'device_name'), [
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'password' => ['required', 'string', 'min:8', 'regex:/[a-z]/', 'regex:/[A-Z]/', 'regex:/[0-9]/', 'regex:/[@$!%*?&#]/'],
+                'device_name' => 'required'
             ])->validate();
 
             $user = User::create([
@@ -29,7 +30,12 @@ class AuthController extends Controller
                 'password' => Hash::make($request['password']),
             ]);
 
-            return response()->json(['user' => $user], 201);
+            $token = $user->createToken(
+                $request->device_name,
+                ["read", "update", "create", "delete"]
+            )->plainTextToken;
+
+            return response()->json(['token' => $token, "user" => $user], 201);
         } catch (ValidationException $e) {
             return response()->json(['errors' => $e->errors()], 422);
         }
@@ -37,33 +43,40 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+                'device_name' => 'required',
+            ]);
 
-        if (Auth::attempt($request->only('email', 'password'))) {
+            if (Auth::attempt($request->only('email', 'password'))) {
 
-            $token = Auth::user()->createToken(
-                'auth_token',
-                ["read", "update", "create", "delete"]
-            )->plainTextToken;
+                $token = Auth::user()->createToken(
+                    $request->device_name,
+                    ["read", "update", "create", "delete"]
+                )->plainTextToken;
 
+                return response()->json([
+                    'user' => Auth::user(),
+                    "token" => $token,
+                    'message' => 'Successfully logged in',
+                ], 200);
+            }
             return response()->json([
-                'user' => Auth::user(),
-                "token" => $token,
-                'message' => 'Successfully logged in',
-            ], 200);
+                'message' => 'These credentials do not match our records!',
+            ], 401);
+        } catch (ValidationException $validationException) {
+            return response()->json([
+                'message' => $validationException->getMessage(),
+            ], 401);
         }
-        return response()->json([
-            'message' => 'These credentials do not match our records!',
-        ], 401);
     }
 
     public function logout(Request $request)
     {
         $token = $request->user()->currentAccessToken();
-    
+
         if ($token) {
             $token->delete();
             return response()->json(['message' => 'Token deleted successfully'], 200);
