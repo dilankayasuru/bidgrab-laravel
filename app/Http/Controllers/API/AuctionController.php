ebd1d5f1652b55a56737c45c3961d6d34dda889c
dilankayasuru <dilankayasuru001@gmail.com>
2025-02-04 16:02:13 +0530

    send category name to view

--- app/Http/Controllers/API/AuctionController.php
+++ app/Http/Controllers/API/AuctionController.php
@@ -55,6 +55,7 @@ class AuctionController extends Controller
         }, $auction->images);
 
         $auction->specs = json_decode($auction->specs);
+        $auction->categoryName = $auction->specs->category->name;
 
         return response()->json($auction, 200);
     }
