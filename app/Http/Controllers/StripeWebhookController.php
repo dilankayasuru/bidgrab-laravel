b6e6fa334166640b4a428b7d2b95a40fcbcf038f
dilankayasuru <dilankayasuru001@gmail.com>
2025-02-05 18:22:06 +0530

    webhook fix

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -36,26 +36,6 @@ class StripeWebhookController extends Controller
                 return response()->json(['message' => 'Invalid signature!'], 400);
             }
         }
-
-        if ($event->type == 'checkout.session.completed') {
-            $session = $event->data->object;
-
-            $address = new Address([
-                'city' => $session->shipping_details->address->city,
-                'country'  => $session->shipping_details->address->country,
-                'line1' => $session->shipping_details->address->line1,
-                'line2' => $session->shipping_details->address->line2,
-                'postal_code' => $session->shipping_details->address->postal_code,
-                'phone' => $session->customer_details->phone,
-            ]);
-
-            $order = Order::where('_id', $session->metadata->order_id)->first();
-            $order->address()->associate($address);
-            $order->status = 'payed';
-            $order->payment = $session->payment_intent;
-            $order->save();
-            return response()->json(['status' => 'success'], 200);
-        }
         if ($event->type == "charge.succeeded") {
             $session = $event->data->object;
 
@@ -73,7 +53,7 @@ class StripeWebhookController extends Controller
             $order->status = 'payed';
             $order->payment = $session->payment_intent;
             $order->save();
-            return response()->json(['status' => 'success'], 200);
         }
+        return response()->json(['status' => 'success'], 200);
     }
 }
